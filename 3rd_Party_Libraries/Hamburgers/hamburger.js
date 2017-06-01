3dba6a14abd49df3afeac6b617fc1fe980eb79b2
Cole Davis <Cole Davis>
2017-05-31 22:18:46 -0600

    Changed Hamburger Dropdown styling.

--- 3rd_Party_Libraries/Hamburgers/hamburger.js
+++ 3rd_Party_Libraries/Hamburgers/hamburger.js
@@ -2,11 +2,12 @@ var $hamburger = $(".hamburger");
 $hamburger.on("click", function() {
   $hamburger.toggleClass("is-active");
   if($hamburger.hasClass("is-active")){
-    $('nav').removeClass('animated slideOutLeft').addClass('animated slideInLeft');
+    //Animate the Nav
+    $('nav').removeClass('animated slideOutUp').addClass('animated slideInDown');
     $("nav").css("visibility", "visible");
   }
   else{
-    $('nav').removeClass('animated slideInLeft').addClass('animated slideOutLeft');
+    $('nav').removeClass('animated slideInDown').addClass('animated slideOutUp');
 
   }
 });
