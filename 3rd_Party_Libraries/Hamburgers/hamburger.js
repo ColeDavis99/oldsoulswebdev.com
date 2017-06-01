45b5843b903d01fc67fc87cf5b67697bc1246eda
Cole Davis <Cole Davis>
2017-05-31 18:25:55 -0600

    Added Hamburger Menu Functinality. Don't have any links going anywhere yet though.

--- 3rd_Party_Libraries/Hamburgers/hamburger.js
+++ 3rd_Party_Libraries/Hamburgers/hamburger.js
@@ -1,5 +1,12 @@
 var $hamburger = $(".hamburger");
 $hamburger.on("click", function() {
   $hamburger.toggleClass("is-active");
-  // Do something else, like open/close menu
+  if($hamburger.hasClass("is-active")){
+    $('nav').removeClass('animated slideOutLeft').addClass('animated slideInLeft');
+    $("nav").css("visibility", "visible");
+  }
+  else{
+    $('nav').removeClass('animated slideInLeft').addClass('animated slideOutLeft');
+
+  }
 });
