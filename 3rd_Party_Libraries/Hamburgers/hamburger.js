7286aea0d1320366f01e93c1b20046e4d4d77c0e
Cole Davis <Cole Davis>
2017-06-01 23:35:38 -0600

    Hamburger Navigation Mockup is complete.

--- 3rd_Party_Libraries/Hamburgers/hamburger.js
+++ 3rd_Party_Libraries/Hamburgers/hamburger.js
@@ -1,20 +1,21 @@
-var $hamburger = $(".hamburger");
-$hamburger.on("click", function() {
-  $hamburger.toggleClass("is-active");
-  if($hamburger.hasClass("is-active")){
-    //Animate the Nav
-    $('nav').removeClass('animated slideOutUp').addClass('animated slideInDown');
-    //Make nav visible when button is clicked for the first time.
-    $("nav").css("visibility", "visible");
-    //Make the menu scroll, but not the rest of the page.
-    $("#headerWrapper, #contentWrapper").css("position","fixed");
-
-  }
-  else{
-    $('nav').removeClass('animated slideInDown').addClass('animated slideOutUp');
-    //Make the content scrollable again.
-    $("#headerWrapper, #contentWrapper").css("position","relative");
-
-
-  }
+$( document ).ready(function() {
+  /* Here is the mobile menu logic. */
+  var $hamburger = $(".hamburger");
+  $hamburger.on("click", function() {
+    $hamburger.toggleClass("is-active");
+    if($hamburger.hasClass("is-active")){
+      //Animate the Nav
+      $('nav').removeClass('animated slideOutUp').addClass('animated slideInDown');
+      //Make nav visible when button is clicked for the first time.
+      $("nav").css("visibility", "visible");
+      //Make the menu scroll, but not the rest of the page.
+      $("#headerWrapper, #contentWrapper").css("position","fixed");
+    }
+    else{
+      $('nav').removeClass('animated slideInDown').addClass('animated slideOutUp');
+      //Make the content scrollable again.
+      $("#headerWrapper, #contentWrapper").css("position","absolute");
+    }
+  });
+  //===============End of mobile menu logic===//
 });
