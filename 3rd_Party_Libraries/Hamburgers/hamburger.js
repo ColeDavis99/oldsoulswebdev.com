6b8335d87c5aade29e38728612e9a3a66cf7ad4d
Cole Davis <Cole Davis>
2017-06-04 18:53:23 -0600

    Hamburger Button now closes when you click on the extra space of the expanded hamburger menu.

--- 3rd_Party_Libraries/Hamburgers/hamburger.js
+++ 3rd_Party_Libraries/Hamburgers/hamburger.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  /* Here is the mobile menu logic. */
+  /* Open or Close the hamburger menu*/
   var $hamburger = $(".hamburger");
   $hamburger.on("click", function() {
     $hamburger.toggleClass("is-active");
@@ -12,8 +12,6 @@ $( document ).ready(function() {
       $("main").css("position","fixed");
       //Keeps body's background image from scrolling when nav is open.
       $("body").css("background-attachment", "fixed");
-
-
     }
     else{
       $('nav').removeClass('animated slideInDown').addClass('animated slideOutUp');
@@ -22,5 +20,19 @@ $( document ).ready(function() {
       $("body").css("background-attachment", "scroll");
     }
   });
+
+
+    //CLose navigation if user clicks anywhere that isn't an option in the nav.
+
+      $("main").on("click", function(){
+        $('nav').removeClass('animated slideInDown').addClass('animated slideOutUp');
+        //Make the main content scrollable again.
+        $("main").css("position","relative");
+        $("body").css("background-attachment", "scroll");
+        //Make the hamburger back to it's 3 line position.
+        $hamburger.removeClass("is-active");
+      });
+
+
   //===============End of mobile menu logic===//
 });
