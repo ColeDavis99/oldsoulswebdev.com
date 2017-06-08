8677c879c34ac88c611eb8ca09eb991fed19d7d9
Cole Davis <Cole Davis>
2017-06-08 16:57:21 -0600

    Fixed About Us Dropdown Bug, helping Zach with ScrollMagic logic in main.js

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -8,4 +8,11 @@ $(document).ready(function(){
     })
     .setClassToggle('#about', 'fade-in') //add class to elem with about ID
     .addTo(controller);
+
+
+    //Here is your second thing you were trying to do Zach.
+    var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 300})
+            .setPin("#pin1")
+            .addIndicators({name: "1 (duration: 300)"}) // add indicators (requires plugin)
+            .addTo(controller);
 });
