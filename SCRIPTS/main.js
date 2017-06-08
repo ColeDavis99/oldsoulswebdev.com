1bf7f98007ea5fb1b326a0ea40d8c6dfd0e15f92
Rotheron <lilbing99@gmail.com>
2017-06-08 17:09:43 -0500

    fixed bug in the fade-in

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -3,16 +3,39 @@ $(document).ready(function(){
     var controller = new ScrollMagic.Controller();
 
     // build a scene
-    var ourScene = new ScrollMagic.Scene({
+    var fadeScene = new ScrollMagic.Scene({
       triggerElement: '#about'
     })
     .setClassToggle('#about', 'fade-in') //add class to elem with about ID
     .addTo(controller);
 
+// ---------------  LARRYS CODE -----------------------------------------
+    // Scene Handler
+    var scene = new ScrollMagic.Scene({
+    triggerElement: "#pinned-trigger1", // point of execution
+    duration: $(window).height() - 100, // pin element for the window height - 1
+    triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
+    reverse: true // allows the effect to trigger when scrolled in the reverse direction
+    })
+    .setPin("#pinned-element1"); // the element we want to pin
 
-    var ourScene2 = new ScrollMagic.Scene({
+    // Scene2 Handler
+    var scene2 = new ScrollMagic.Scene({
+    triggerElement: "#pinned-trigger2", // point of execution
+    duration: 400 // pin the element for a total of 400px
+    })
+    .setPin("#pinned-element2"); // the element we want to pin
+
+    // Add Scenes to ScrollMagic Controller
+    controller.addScene([
+    scene,
+    scene2
+    ]);
+
+    // This is the function for sliding in the why choose us reasons
+    var fadeScene2 = new ScrollMagic.Scene({
       triggerElement: '#pinned-trigger2'
     })
-    ScrollMagic.Scene({triggerElement: "#trigger1"})
-                        .addTo(controller);
+    .setClassToggle('#', 'fade-in') //add class to elem with about ID
+    .addTo(controller);
 });
