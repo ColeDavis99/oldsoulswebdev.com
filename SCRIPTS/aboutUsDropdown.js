b91b0267d3de682f7a370cedae6bc20eac9376f8
Cole Davis <Cole Davis>
2017-06-07 23:12:48 -0600

    Added additional 'about us' dropdown functionality inside SCRIPTS/aboutUsDropdown.js. Also moved hamburger.js into the SCRIPTS folder.

--- SCRIPTS/aboutUsDropdown.js
+++ SCRIPTS/aboutUsDropdown.js
@@ -1,5 +1,21 @@
+//This script handles the "aboutUs" dropdowns that display information about us.
 $(document).ready(function(){
+
+  //When our image is clicked
   $('.mugshot').on("click", function(){
     $(this).next().next().slideToggle();
+    lastClicked = $(this);
+  });
+
+  //Close .desc when user clicks on #about blank space, unless hamburger menu is open (design choice)
+  var $nav = $('nav')
+  $('#about').click(function(e){
+    if($(e.target).is('.mugshot, h3, span')){
+      e.preventDefault();
+      return;
+    }
+    if($nav.hasClass("slideOutUp")){
+      lastClicked.next().next().slideToggle();
+    };
   });
 });
