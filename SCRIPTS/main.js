fa5094f4ce741aa7eb7f2f550417156adfe99d1c
Rotheron <lilbing99@gmail.com>
2017-06-08 16:57:19 -0500

    commiting changes

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -8,4 +8,10 @@ $(document).ready(function(){
     })
     .setClassToggle('#about', 'fade-in') //add class to elem with about ID
     .addTo(controller);
+
+    var ourScene2 = new ScrollMagic.Scene({
+      triggerElement: '#pinned-trigger2'
+    })
+    ScrollMagic.Scene({triggerElement: "#trigger1"})
+                        .addTo(controller);
 });
