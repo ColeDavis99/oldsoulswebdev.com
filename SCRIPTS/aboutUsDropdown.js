f2b9cb47eb8e242a602b81598c1b41146a5e31e2
Cole Davis <Cole Davis>
2017-06-08 23:04:14 -0600

    Couldn't get the scroll-to clicked aboutUs working properly, so I said screw it. It looks pretty good as it is.

--- SCRIPTS/aboutUsDropdown.js
+++ SCRIPTS/aboutUsDropdown.js
@@ -2,13 +2,18 @@
 //Only One of us Can Be Expanded at the Same Time (design choice)
 $(document).ready(function(){
 
-  //When our image is clicked
+  //When our image is clicked...
   $('.mugshot').on("click", function(){
     expandedPerson = $(this).next().next();
-    expandedPerson.slideToggle();
 
+    //...Close all other .descs, except for the one just clicked
     $(".desc").not(expandedPerson).slideUp();
+
+    //And open our clicked .desc
+    expandedPerson.delay(300).slideToggle();
   });
+
+
 
   //Close .desc when user clicks on #about blank space, unless hamburger menu is open (design choice)
   var $nav = $('nav')
